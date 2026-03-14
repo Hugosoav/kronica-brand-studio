325008390ccf7745938c7b3e149d898288394952
Hugosoav <hugosoav@gmail.com>
2026-03-14 18:43:36 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/nome-do-seu-repositorio/',
   server: {
     host: "::",
     port: 8080,
