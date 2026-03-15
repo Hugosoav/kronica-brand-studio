666017f2494dcc4ad48b42a32a84365f16180e24
Hugosoav <hugosoav@gmail.com>
2026-03-15 19:33:34 -0300

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -104,7 +104,6 @@ const Header = () => {
                   src={logoBranco}
                   alt="Kronica"
                   className="h-5 md:h-6"
-                  whileHover={{ scale: 1.05 }}
                   transition={{ type: "spring", stiffness: 400, damping: 15 }}
                 />
               </Link>
